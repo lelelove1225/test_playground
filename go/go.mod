10f6d38999fb0c9c95adda5fc12ecfb854f39471
Charlotte_Jeremiah <40084865+lelelove1225@users.noreply.github.com>
2021-12-17 01:30:29 +0900

    test#1

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/lelelove1225/test_playground/go
+
+go 1.16
---
+++ go/loop.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	//"net/http"
+)
+
+func main() {
+
+	url_front_string := "https://ja.wikipedia.org/wiki/"
+	middle_url := 0
+
+	fmt.Println(url_front_string)
+	fmt.Println((middle_url))
+}
